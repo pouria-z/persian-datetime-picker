755fd6d0029f67398ab8a17a3db4e30f23d4d597
pouria-z <pouria.zeinalzadeh@gmail.com>
2023-03-28 18:34:43 +0330

    jump to end of screen

--- lib/src/pcalendar_date_range_picker.dart
+++ lib/src/pcalendar_date_range_picker.dart
@@ -88,8 +88,8 @@ class _PCalendarDateRangePickerState extends State<PCalendarDateRangePicker> {
     super.initState();
     _controller = ScrollController();
     _controller!.addListener(_scrollListener);
-    Future.delayed(const Duration(milliseconds: 1000), () {
-      _controller!.jumpTo(_controller!.position.maxScrollExtent);
+    Future.delayed(const Duration(milliseconds: 250), () {
+      _controller!.jumpTo(_controller!.position.maxScrollExtent*2);
     });
 
     _startDate = widget.initialStartDate;
